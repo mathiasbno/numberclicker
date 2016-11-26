0d2d069add21f7b0920be4ea09bd1ad61a449e12
Mathias Benjaminsen <mabenjam@gmail.com>
2016-11-26 15:34:20 +0100

    Add port variable

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
   res.sendFile( __dirname + "/" + "index.html" );
 })
 
-var server = app.listen(2000, function () {
+var server = app.listen(process.env.PORT || 2000, function () {
   var host = server.address().address
   var port = server.address().port
 
